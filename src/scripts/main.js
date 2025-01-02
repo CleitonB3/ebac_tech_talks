52322a617f1bfa5ec6e6cc3d3983a68860d120e8
CleitonB3 <ville3003@gmail.com>
2025-01-02 20:31:35 -0300

    criando a contagem regressiva

---
+++ src/scripts/main.js
@@ -0,0 +1,29 @@
+AOS.init();
+
+const dataDoEvento = new Date("Jan 12, 2025 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaDoEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24
+    const horaEmMs = 1000 * 60 * 60
+    const minutoEmMs = 1000 * 60
+
+    const diasAteOEvento = Math.floor(distanciaDoEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distanciaDoEvento % diaEmMs) / horaEmMs) ;
+    const minutosAteOEvento = Math.floor((distanciaDoEvento % horaEmMs) / minutoEmMs) ;
+    const segundosAteOEvento = Math.floor((distanciaDoEvento % minutoEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+    if(distanciaDoEvento < 0){
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'Evento expirado';
+    }
+}, 1000);
+
+
